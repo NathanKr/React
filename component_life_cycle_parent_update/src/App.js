2b363e74da35aecdfee757f4e8e7ee665b754e3f
Nathan Krasney Laptop <natankrasney@gmail.com>
2018-02-25 11:47:26 +0200

    sample code to see life cycle changes for create and update by parent

---
+++ component_life_cycle_parent_update/src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './App.css';
+import Child from './Child/Child'
+
+class App extends Component {
+  state = {text : 'text1'};
+ constructor(props){
+    super(props);  
+     console.log('App.js constructor');
+    }
+
+ componentWillMount(){ 
+   console.log('App.js componentWillMount');
+  }
+ 
+  buttonClickHandler = () => {
+    const newText = this.state.text === 'text1' ? 'text2' : 'text1';
+    this.setState({text : newText});
+  }
+render() {
+   console.log('App.js render');
+   return (<div className="App">
+       <h1>React</h1>
+       <button onClick={this.buttonClickHandler}>Click to change state</button>
+       <Child text={this.state.text}/>
+     </div>);}
+
+ componentDidMount(){ 
+   console.log('App.js componentDidMount');
+  }
+
+  componentWillReceiveProps(nextProps)
+  {
+      console.log('UPDATE App.js componentWillReciveProps . nextProps : ');
+      console.log(nextProps);
+  }
+
+  shouldComponentUpdate(nextProps,nextState)
+  {
+      console.log('UPDATE App.js shouldComponentUpdate return true' +
+                  '\nnextProps : ' + JSON.stringify(nextProps) + 
+                  '\nnext state : ' + JSON.stringify(nextState) );
+      return true;
+  }
+
+  componentWillUpdate(nextProps,nextState)
+  {
+      console.log('UPDATE App.js componentWillUpdate' +
+      '\nnextProps : ' + JSON.stringify(nextProps) + 
+      '\nnext state : ' + JSON.stringify(nextState) );
+  }
+
+
+  componentDidUpdate(){
+        console.log('App.js componentDidUpdate');
+    }
+}
+export default App;
+
+
