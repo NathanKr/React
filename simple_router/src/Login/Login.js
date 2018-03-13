abbc202c8aa89d0eb22902fe08b98b68c65dad55
Nathan Krasney <natankrasney@gmail.com>
2018-03-13 19:13:12 +0200

    fix merge issues

--- simple_router/src/Login/Login.js
+++ simple_router/src/Login/Login.js
@@ -1,6 +1,5 @@
 import React , { Component } from 'react';
 
-<<<<<<< HEAD
 class Login extends Component {
     componentDidMount()
     {
@@ -10,13 +9,7 @@ class Login extends Component {
     render() {
       return <h2>Login</h2>;
     }
-=======
-
-function Login(props)
-{
-    console.log(props);
-    return <h2>Login</h2>;
->>>>>>> fd4e14d11bbedd06d9475d35e3a83b526ea57087
 }
+
 
 export default Login;
