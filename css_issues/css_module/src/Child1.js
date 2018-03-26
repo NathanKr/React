d5364db4aaf6daa6582b900cc1451910ee9b7870
Nathan Krasney Laptop <natankrasney@gmail.com>
2018-03-26 13:20:02 +0300

    initial version

---
+++ css_issues/css_module/src/Child1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Child1.css' 
+
+function Child1()
+{
+    return <p className={styles.Child}>child1</p>
+}
+
+export default Child1
---
+++ css_issues/css_module/src/Child2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Child2.css' 
+
+
+function Child2()
+{
+    return <p className={styles.Child}>child2</p>
+}
+
+export default Child2
