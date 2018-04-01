f4cc7aeafc9f7b5cc0187c6d41e44cb59d903519
Nathan Krasney Laptop <natankrasney@gmail.com>
2018-04-01 11:25:56 +0300

    small file format change

--- Redux/pure_redux/src/index.js
+++ Redux/pure_redux/src/index.js
@@ -1,21 +1,32 @@
 import {createStore} from 'redux'
-const addType = "ADD";const mulType = "MUL"; let initialState = {count : 0};
+const addType = "ADD";
+const mulType = "MUL";
+ let initialState = {count : 0};
+
 const reducer = (state,action) =>{
     let newCount;
    switch(action.type){
        case addType :
-       newCount = state.count + action.payLoad;break;
+       newCount = state.count + action.payLoad;
+       break;
+
        case mulType :
-       newCount = state.count * action.payLoad;break;
+       newCount = state.count * action.payLoad;
+       break;
+
        default :
        newCount=state.count;
-   }return {...state , count: newCount};
+   }
+   return {...state , count: newCount};
 }
 
 const store = createStore(reducer,initialState);
-store.subscribe( () =>{console.log("store has changed",store.getState());} );
+store.subscribe( () =>{
+    console.log("store has changed",store.getState());
+} );
 // --- action MUST be defined with type as key
-store.dispatch({type : addType , payLoad : 3});store.dispatch({type : mulType , payLoad : 2});
+store.dispatch({type : addType , payLoad : 3});
+store.dispatch({type : mulType , payLoad : 2});
 store.dispatch({type : addType , payLoad : -1});
 
  
