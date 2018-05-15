30931f9a2098da01f0efe9b721d0cb53da3e5f81
Nathan Krasney <natankrasney@gmail.com>
2018-05-15 13:14:58 +0300

    add also App for use with styles

--- react_course_online/styles/css_modules/src/App.js
+++ react_course_online/styles/css_modules/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 import Child2 from './Child2';
 import Child1 from './Child1';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.App}>
         <Child2 />
         <Child1 />
       </div>
