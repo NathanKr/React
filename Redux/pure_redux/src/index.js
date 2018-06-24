d964e8a6112bc7623a05836582521f372cf1718f
Nathan Krasney <natankrasney@gmail.com>
2018-06-24 10:32:20 +0300

    directory change

--- Redux/pure_redux/src/index.js
+++ Redux/pure_redux/src/index.js
@@ -17,13 +17,18 @@ const reducer = (state,action) =>{
        default :
        newCount=state.count;
    }
+   // --- in this case ...state does nothing because it has only count.
+   // --- but its here because in general it should be used
    return {...state , count: newCount};
 }
 
 const store = createStore(reducer,initialState);
+
+// --- subscribe is optional
 store.subscribe( () =>{
     console.log("store has changed",store.getState());
 } );
+
 // --- action MUST be defined with type as key
 store.dispatch({type : addType , payLoad : 3});
 store.dispatch({type : mulType , payLoad : 2});
