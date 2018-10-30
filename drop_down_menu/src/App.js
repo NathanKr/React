8cb9e977de2195c207732e87f94ae7d1787f9534
Nathan Krasney <natankrasney@gmail.com>
2018-10-30 14:18:04 +0200

    bug still exist

--- drop_down_menu/src/App.js
+++ drop_down_menu/src/App.js
@@ -2,25 +2,23 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  state = { onButton: false, onMenu: false };
+  state = {
+    showMenu: false,
+  };
 
   render() {
-    const shouldShow = () => {
-      return this.state.onButton | this.state.onMenu;
-    };
-    console.log("opacity : ", shouldShow());
     const menu = (
       <ul
-        style={{ opacity: shouldShow() ? 1 : 0, transition: "opacity 1s" }}
-        onMouseEnter={() => {
-          console.log("mouse enter menu");
-          this.setState({ ...this.state, onMenu: true });
+        style={{
+          position: "absolute",
+          opacity: this.state.showMenu ? 1 : 0,
+          transition: "opacity 2s"
         }}
         onMouseLeave={() => {
-          console.log("mouse leave menu");
-          /* for some reason moving from button to menu does not cause onMouseLeave on button , 
-          so i am fixing this by setting onButton: false */
-          this.setState({ ...this.state, onMenu: false, onButton: false });
+          this.setState({
+            ...this.state,
+            showMenu: false,
+          });
         }}
       >
         <li>
@@ -39,17 +37,22 @@ class App extends Component {
     );
 
     return (
-      <div>
-        <h2>hover over the button to show the drop down menu</h2>
-        <h2>move the mouse from the menu\button will hide the drop down menu</h2>
-        <button style={{marginBottom:'10px'}}
-          onMouseEnter={() => {
-            console.log("mouse enter button");
-            this.setState({ ...this.state, onButton: true });
+      <div className="App">
+        {/*<h2>hover over the button to show the drop down menu</h2>
+        <h2>
+          move the mouse from the menu\button will hide the drop down menu
+        </h2>*/}
+        <button
+          style={{ marginBottom: "10px" }}
+          onMouseEnter={(info) => {
+            console.log(info);
+            this.setState({
+              ...this.state,
+              showMenu: true,
+            });
           }}
           onMouseLeave={() => {
-            console.log("mouse leave button");
-            this.setState({ ...this.state, onButton: false });
+            this.setState({ ...this.state, showMenu: false });
           }}
         >
           Hover Me
