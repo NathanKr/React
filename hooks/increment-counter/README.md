b2601c87b764978dc8c69c97acc1657fa2a599fd
Nathan Krasney <natankrasney@gmail.com>
2019-02-18 10:56:55 +0200

    use useEffect

--- hooks/increment-counter/README.md
+++ hooks/increment-counter/README.md
@@ -1,6 +1,13 @@
 <h2>Introduction</h2>
 Simple counter functional component using state via hooks
 
+<h2>Hooks used</h2>
+<ul>
+<li>useState</li>
+<li>useEffect</li>
+</ul>
+
+
 <h2>Points of interest</h2>
 <ul>
 <li>eslint-plugin-react-hooks was added to warn about voilating hooks constraints. This plugin will be part of create-react-app in the future by default</li>
--- hooks/increment-counter/src/Counter.js
+++ hooks/increment-counter/src/Counter.js
@@ -1,17 +1,23 @@
-import React , {useState} from 'react';
+import React, { useState, useEffect } from "react";
 
 const Counter = () => {
-    const [count1, setCount1] = useState(0);
-    const [count2, setCount2] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-    return (
-        <div>
-            <button onClick={() => setCount1(count1+1)} >Increment</button>            
-            <p>counter1 : {count1}</p>
-            <button onClick={() => setCount2(count2+1)} >Increment</button>            
-            <p>counter2 : {count2}</p>
-        </div>
-    );
+  useEffect(() => {
+    document.title = `cnt : ${count1} , ${count2}`;
+    return () => console.log("clean up");// -- it is not must to return
+  });
+
+  return (
+    <div>
+      <h2>app title changes on counter change</h2>
+      <button onClick={() => setCount1(count1 + 1)}>Increment</button>
+      <p>counter1 : {count1}</p>
+      <button onClick={() => setCount2(count2 + 1)}>Increment</button>
+      <p>counter2 : {count2}</p>
+    </div>
+  );
 };
 
 export default Counter;
