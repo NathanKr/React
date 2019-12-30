402d06c71031a55fce9035f0f46f193236701a8d
Nathan Krasney <natankrasney@gmail.com>
2019-12-30 13:03:16 +0200

    add render to routing

--- react_course_online/routing/routing_with_arguments/src/App.js
+++ react_course_online/routing/routing_with_arguments/src/App.js
@@ -6,6 +6,7 @@ import Child1 from './Child1';
 import Child2 from './Child2';
 import Child3 from './Child3';
 import Child4 from './Child4';
+import Child5 from './Child5';
 
 import NotFound from './NotFound';
 
@@ -19,12 +20,14 @@ class App extends Component {
         <Link to='/Child2?arg1=15&arg2=16'>Child2</Link>
         <Link to='/Child3'>Child3</Link>
         <Link to='/Child4'>Child4</Link>
+        <Link to='/Child5'>Child5</Link>
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/Child2' component={Child2}/>
           <Route exact path='/Child1/:number' component={Child1}/>
           <Route exact path='/Child3' component ={Child3}/>
           <Route exact path='/Child4' component ={Child4}/>
+          <Route exact path='/Child5' render ={() => <Child5 name='Krasney'/>}/>
           <Route component={NotFound}/>
         </Switch>
         </div>
---
+++ react_course_online/routing/routing_with_arguments/src/Child5.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Child5 = props => {
+    return (
+        <div>
+            <h2>Child5</h2>
+            <p>{props.name}</p>
+        </div>
+    );
+};
+
+export default Child5;
