1538ec9b04f7929612a1961a1c2c40fe04e67c8f
Nathan Krasney <natankrasney@gmail.com>
2021-12-06 08:39:40 +0200

    fix warning

--- spa/router_with_arguments/src/App.js
+++ spa/router_with_arguments/src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
             <Link to={'/'}>Home</Link>
-            <Link to={'/Child1/3'}>Child1->3</Link>
-            <Link to="/Child2?arg1=5&arg2=6">Child2->5,6</Link>
+            <Link to={'/Child1/3'}>Child1-{'>'}3</Link>
+            <Link to="/Child2?arg1=5&arg2=6">Child2-{'>'}5,6</Link>
         <hr />
           <Switch>
             <Route exact path='/' component={Home} />
