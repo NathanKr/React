dafef61abcae03a536a3baf39080a9e2170921ad
Nathan Krasney <natankrasney@gmail.com>
2021-12-29 10:55:53 +0200

    move auth to 2 repo

--- Authentication/authentication_server_firebase/src/Components/Constants.js
+++
@@ -1,9 +0,0 @@
-class Constants{
-    static UrlBase = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/';
-    
-
-    static ApiKey = 'AIzaSyCQpWjPQ8R30S2pbfUvp5uu4EqpRobaEps';
-     
-}
-
-export default Constants;
--- Authentication/authentication_server_firebase/src/Components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Home(props){
-
-
-    return <p>Home component</p>;
-}
-
-export default Home;
